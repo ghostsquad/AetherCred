dfeaad860b4c02746bad9903b75d905ffb577458
Weston McNamee <ghost.squadron@gmail.com>
2015-02-03 09:06:29 -0800

    migrating from PoshBox

---
+++ GpCred.psm1
@@ -0,0 +1,12 @@
+$ErrorActionPrefence = "Stop"
+Set-StrictMode -Version Latest
+
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+[Void](Add-Type -Path $here\CredentialManagement.dll)
+
+. $here\functions\Get-ManagedCredential.ps1
+. $here\functions\Remove-ManagedCredential.ps1
+. $here\functions\Set-ManagedCredential.ps1
+
+Export-ModuleMember -Function *-*
